909cf0bd07070d35b4adf7574fcaa95db0ec2e14
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-27 19:25:14 +0300

    Начало работы над Заданием1. Реализована визуальная часть формы

---
+++ Zadanie1Deutcsh/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie1Deutcsh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+                private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Ваш код для обработки кнопки "Преобразовать"
+            // Например, можно получить значение из InputTextBox и выполнить преобразование.
+            // Затем результат можно установить в OutputTextBox.
+        }
+    }
+}
